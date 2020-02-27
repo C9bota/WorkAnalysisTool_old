68f51b6d312765b8667ecb0b9c73ae50243c0dd2
chie kubota <16rd050@ms.dendai.ac.jp>
2020-02-27 16:40:12 +0900

    TextView と　radioGroupを配列化した。3項目についてチェック結果をログに出力

--- WA_UIprototype/app/src/main/java/com/example/mdd/wa_uiprototype/MainActivity.java
+++ WA_UIprototype/app/src/main/java/com/example/mdd/wa_uiprototype/MainActivity.java
@@ -13,31 +13,54 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     //Ui
-    private RadioGroup rg1;
-    private RadioGroup rg2;
-    private RadioGroup rg3;
+//    private RadioGroup rg1;
+//    private RadioGroup rg2;
+//    private RadioGroup rg3;
+//
+    private RadioGroup[] radioGroups = new RadioGroup[3];
+    private TextView[] textViews = new TextView[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rg1 = findViewById(R.id.radioGroup1);
-        rg2 = findViewById(R.id.radioGroup2);
-        rg3 = findViewById(R.id.radioGroup3);
+//        rg1 = findViewById(R.id.radioGroup1);
+//        rg2 = findViewById(R.id.radioGroup2);
+//        rg3 = findViewById(R.id.radioGroup3);
+
+        radioGroups[0] = findViewById(R.id.radioGroup1);
+        radioGroups[1] = findViewById(R.id.radioGroup2);
+        radioGroups[2] = findViewById(R.id.radioGroup3);
+
+        textViews[0] = findViewById(R.id.textView);
+        textViews[1] = findViewById(R.id.textView2);
+        textViews[2] = findViewById(R.id.textView3);
+
 
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //radio groupらの値を拾ってくる
-                int checkedId = rg1.getCheckedRadioButtonId();
-                if(checkedId != -1){
-                    RadioButton radioButton = findViewById(checkedId);
-                    TextView textView = findViewById(R.id.textView);
-                    String textgp = textView.getText().toString();
-                    String textbt = radioButton.getText().toString();
-                    Log.v("チェック済み",textgp + " " + textbt);
+//                int checkedId = rg1.getCheckedRadioButtonId();
+//                if(checkedId != -1){
+//                    RadioButton radioButton = findViewById(checkedId);
+//                    TextView textView = findViewById(R.id.textView);
+//                    String textgp = textView.getText().toString();
+//                    String textbt = radioButton.getText().toString();
+//                    Log.v("チェック済み",textgp + " " + textbt);
+//                }
+
+                for(int i=0; i<radioGroups.length; i++){
+                    int checkedId = radioGroups[i].getCheckedRadioButtonId();
+                    if(checkedId != -1){
+                        RadioButton radioButton = findViewById(checkedId);
+
+                        String textgp = textViews[i].getText().toString();
+                        String textbt = radioButton.getText().toString();
+                        Log.v("チェック済み",textgp + " " + textbt);
+                    }
                 }
             }
         });
