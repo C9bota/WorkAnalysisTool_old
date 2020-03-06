a35bd62d3ee734a40af9647429934c2219ea764b
chie kubota <16rd050@ms.dendai.ac.jp>
2020-03-06 16:26:21 +0900

    outputFragmentでのCSV表示の実装途中　premission denayedでなぜか断られる

--- WA_ResponsibleTest/app/src/main/java/com/example/wa_responsibletest/MainActivity.java
+++ WA_ResponsibleTest/app/src/main/java/com/example/wa_responsibletest/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager pager;
     private FragmentPagerAdapter adapter;
     private int curretPage;
+
+    private TextView textViewCSV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +32,36 @@ public class MainActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
         curretPage = 0;
 
+        textViewCSV = findViewById(R.id.text_view_output);
 
+        writeToCSVFile();
+        readFromCSVFile();
+    }
 
+    
+    //読み出し全般
+    public final String FILE_NAME = "test.csv";
+    void readFromCSVFile(){
+        //読み出し
+        FileInputStream fis;
+        try {
+            fis = openFileInput(FILE_NAME);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(fis, StandardCharsets.UTF_8));
+            String str ="";
+            while(reader.readLine() != null){
+                str += reader.readLine() +"\n";
+                textViewCSV.setText(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 
 
 }
--- WA_ResponsibleTest/app/src/main/java/com/example/wa_responsibletest/OutputFragment.java
+++ WA_ResponsibleTest/app/src/main/java/com/example/wa_responsibletest/OutputFragment.java
@@ -9,16 +9,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 
 public class OutputFragment extends Fragment {
 
+//    private TextView textViewCSV;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_output, container, false);
+        View v = inflater.inflate(R.layout.fragment_output, container, false);
+//        textViewCSV = v.findViewById(R.id.text_view_output);
+//        readFromCSVFile();
+        return v;
     }
+
+//    //読み出し全般
+//    public final String FILE_NAME = "test.csv";
+//    void readFromCSVFile(){
+//        //読み出し
+//        FileInputStream fis;
+//        try {
+//            fis = getActivity().openFileInput(FILE_NAME);
+//
+//            BufferedReader reader = new BufferedReader(
+//                    new InputStreamReader(fis, StandardCharsets.UTF_8));
+//            String str ="";
+//            while(reader.readLine() != null){
+//                str += reader.readLine() +"\n";
+//                textViewCSV.setText(str);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
 
 }
